f559aa4b1a9024d4b9a74522086408dec1f1a896
fernandopa <fernandopa@epidataconsulting.com>
2023-06-11 18:35:22 -0300

    agregar y eliminar medios de pago

--- src/doms.js
+++ src/doms.js
@@ -24,3 +24,30 @@ const modal = document.getElementById("myModal");
 const buttonGoToLogin = document.getElementById("buttonGoToLogin")
 
 const medioDePagoContainer = document.getElementById("medioDePagoContainer")
+
+const modalTarjeta = document.getElementById("myModalTarjeta");
+const myModalMedioPagoDigital = document.getElementById("myModalMedioPagoDigital");
+
+const agregarTarjetaBtn = document.getElementById("agregarTarjetaBtn")
+const agregarMedioDigitalBtn = document.getElementById("agregarMedioDigitalBtn")
+
+const closeMyModalTarjeta = document.getElementById("closeMyModalTarjeta")
+const closeMyModalMedioPagoDigital = document.getElementById("closeMyModalMedioPagoDigital")
+
+
+// modal de nueva tarjeta
+const inputNombreTrjeta = document.getElementById("inputNombreTrjeta")
+const inputFechaVencimientoNuevaTarjeta = document.getElementById("inputFechaVencimientoNuevaTarjeta")
+const inputNumeroTarjeta = document.getElementById("inputNumeroTarjeta")
+
+const errorNewtarjeta = document.getElementById("errorNewtarjeta")
+
+const buttonSaveTarjeta = document.getElementById("buttonSaveTarjeta")
+
+// modal de nuevo medio digital
+const inputNombreMedioDigital = document.getElementById("inputNombreMedioDigital")
+const inputEmailMedioDigital = document.getElementById("inputEmailMedioDigital")
+const inputCVUMedioDigital = document.getElementById("inputCVUMedioDigital")
+
+const buttonSaveMedioDigital = document.getElementById("buttonSaveMedioDigital")
+const errorNewMedioDigital = document.getElementById("errorNewMedioDigital")
--- src/usuario.js
+++ src/usuario.js
@@ -20,7 +20,7 @@ templateMedioDePagoDigital = `
 
 templateTarjeta = `
 <div class="avisoCardContainer">
-<h3 class="aviso_name_card">Medio de Pago digital</h3>
+<h3 class="aviso_name_card">Tarjeta de crédito</h3>
 <span onClick="deleteMedio(event)" order="{order}" class="close">&times;</span>
 <div class="body_aviso_card">
 <div class="datos_container_card">
@@ -84,7 +84,23 @@ const renderMedioDePago = (mediosDePago) => {
 
 
 
+const saveCuentas = (cuentas) => {
+  const miembrosJsonString = JSON.stringify(cuentas);
+    
+  localStorage.setItem("miembros", miembrosJsonString); 
 
+  storedStringUsers = localStorage.getItem("miembros");
+  cuentas = JSON.parse(storedStringUsers);
+
+  for(let i=0; i<cuentas.length; i++){
+    if(cuentas[i].email === tokenUser){
+      cuenta = cuentas[i] 
+    }
+  }
+
+  renderMedioDePago(cuenta.mediosDePago)
+
+}
 
 if(!tokenUser){
   modal.style.display = "block";
@@ -109,10 +125,6 @@ if(!tokenUser){
 
 buttonGoToLogin.onclick = function(){
   window.location.href = "login.html"
- 
- 
- 
-
 }
 
 const deleteMedio = (event) => {
@@ -135,4 +147,139 @@ const deleteMedio = (event) => {
 
   renderMedioDePago(cuenta.mediosDePago)
 
+}
+
+agregarTarjetaBtn.onclick = function(){
+  modalTarjeta.style.display="block"
+} 
+
+agregarMedioDigitalBtn.onclick = function(){
+  myModalMedioPagoDigital.style.display="block"
+} 
+
+closeMyModalMedioPagoDigital.onclick = function(){
+  myModalMedioPagoDigital.style.display="none"
+}
+
+closeMyModalTarjeta.onclick = function(){
+  modalTarjeta.style.display="none"
+}
+
+function esNumeroTarjetaValido(numeroTarjeta) {
+  // Eliminar los espacios en blanco y guiones del número de tarjeta
+  var numeroLimpio = numeroTarjeta.replace(/\s/g, "").replace(/-/g, "");
+
+  // Verificar que el número de tarjeta contenga solo dígitos y tenga una longitud válida
+  if (!/^\d+$/.test(numeroLimpio) || numeroLimpio.length < 13 || numeroLimpio.length > 19) {
+    return false;
+  }
+
+  // Aplicar el algoritmo de Luhn para verificar la validez del número de tarjeta
+  var suma = 0;
+  var doblar = false;
+  for (var i = numeroLimpio.length - 1; i >= 0; i--) {
+    var digito = parseInt(numeroLimpio.charAt(i), 10);
+    if (doblar) {
+      digito *= 2;
+      if (digito > 9) {
+        digito -= 9;
+      }
+    }
+    suma += digito;
+    doblar = !doblar;
+  }
+  return suma % 10 === 0;
+}
+
+const validarTarjetaModal = () => {
+  let warnings = "";
+  errorNewtarjeta.innerHTML = "";
+  
+  if(!inputNombreTrjeta.value){
+    warnings += "Nombre  de tarjeta inválido<br>"
+  }
+
+  if(inputNumeroTarjeta.value===""){
+    warnings += "Falta ingresar número de tarjeta<br>"
+  }
+  else if(!esNumeroTarjetaValido(inputNumeroTarjeta.value)){
+    warnings += "Número de tarjeta inválido<br>"
+  }
+  if(inputFechaVencimientoNuevaTarjeta.value < new Date()){
+    warnings += "La tarjeta esta expirada<br>"
+
+  }
+  return warnings
+}
+
+
+function esCVUValido(cvu) {
+  // Eliminar los espacios en blanco del CVU
+  var cvuLimpio = cvu.replace(/\s/g, "");
+
+  // Verificar que el CVU contenga solo dígitos y tenga una longitud válida
+  if (!/^\d+$/.test(cvuLimpio) || cvuLimpio.length !== 22) {
+    return false;
+  }
+
+  // Verificar el dígito de control del CVU
+  var digitoControl = parseInt(cvuLimpio.charAt(cvuLimpio.length - 1), 10);
+  var cvuSinDigitoControl = cvuLimpio.slice(0, -1);
+  var suma = 0;
+  var multiplicador = 7;
+
+  for (var i = cvuSinDigitoControl.length - 1; i >= 0; i--) {
+    var digito = parseInt(cvuSinDigitoControl.charAt(i), 10);
+    suma += digito * multiplicador;
+    multiplicador = multiplicador === 2 ? 7 : multiplicador - 1;
+  }
+
+  var digitoVerificador = 11 - (suma % 11);
+  if (digitoVerificador === 11) {
+    digitoVerificador = 0;
+  } else if (digitoVerificador === 10) {
+    digitoVerificador = 9;
+  }
+  
+  return digitoControl === digitoVerificador;
+}
+
+const isMailValido = (mail) =>{
+  let validacionMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return validacionMail.test(mail)
+}
+
+const validarMedioDigitalModal = ()=>{
+  let warnings = "";
+  errorNewMedioDigital.innerHTML = ""
+
+  if(!inputNombreMedioDigital.value){
+    warnings += "Nombre  de billetera virtual inválido<br>"
+  }
+
+  if(inputCVUMedioDigital.value===""){
+    warnings += "Falta ingresar CVU<br>"
+  }
+  else if(!esCVUValido(inputCVUMedioDigital.value)){
+    warnings += "CVU de tarjeta inválido<br>"
+  }
+  if(!isMailValido(inputEmailMedioDigital.value)){
+    warnings += "El mail no es válido<br>"
+
+  }
+  return warnings
+}
+
+
+buttonSaveMedioDigital.onclick = (e)=>{
+    let errores = validarMedioDigitalModal();
+    if(errores!=""){
+      errorNewMedioDigital.innerHTML = errores
+    }else{
+      let billeteraVirtual = new BilleteraVirtual(inputNombreMedioDigital.value, inputEmailMedioDigital.value, inputCVUMedioDigital.value)
+      cuenta.mediosDePago.push(billeteraVirtual)
+      
+      saveCuentas(cuentas)
+      myModalMedioPagoDigital.style.display="none"
+    }
 }
